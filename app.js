4534005b01574e162c97d6c8320537738a1530db
Jonathan3x <jonathan3x@outlook.com>
2025-03-06 14:29:05 -0600

    Tecla enter despues de escribir el nombre del amigo

--- app.js
+++ app.js
@@ -1,50 +1,54 @@
-let amigos = []; 
+let amigos = [];
 
-function agregarAmigo(){
-  let amigoUsuario = document.getElementById("amigo"); 
-  let valorAmigo = amigoUsuario.value.trim(); 
+function agregarAmigo() {
+  let amigoUsuario = document.getElementById("amigo");
+  let valorAmigo = amigoUsuario.value.trim();
 
   if (valorAmigo === "") {
-    alert("Por favor, escribe un nombre para comenzar a jugar"); 
+    alert("Por favor, escribe un nombre para comenzar a jugar");
     return;
   }
 
-  amigos.push(valorAmigo); 
-  limpiarInputUsuario(); 
+  amigos.push(valorAmigo);
+  limpiarInputUsuario();
   amigoUsuario.focus();
   crearLista();
   console.log(amigos);
 }
 
-function crearLista (){
-listaAmigos = document.getElementById("listaAmigos");
-listaAmigos.innerHTML = "";
+function crearLista() {
+  listaAmigos = document.getElementById("listaAmigos");
+  listaAmigos.innerHTML = "";
 
-for(let i=0; i<amigos.length; i++){ 
+  for (let i = 0; i < amigos.length; i++) {
     let crearLista = document.createElement("li");
-        crearLista.textContent = amigos[i];
-        listaAmigos.appendChild(crearLista);
+    crearLista.textContent = amigos[i];
+    listaAmigos.appendChild(crearLista);
   }
-} 
+}
 
-function sortearAmigo(){
-
-  if (amigos.length <2) {
+function sortearAmigo() {
+  if (amigos.length < 2) {
     alert("No hay amigos para sortear");
     return;
-  }                                 
+  }
 
   let resultadoAmigos = amigos[Math.floor(Math.random() * amigos.length)];
   let resultado = document.getElementById("resultado");
   resultado.innerHTML = `El amigo sorteado es: ${resultadoAmigos}`;
-  
+
   let limpiarLista = document.getElementById("listaAmigos");
-  limpiarLista.innerHTML ="";
+  limpiarLista.innerHTML = "";
 
   document.getElementById("botonSortear").disabled = true;
-  
 }
 
-function limpiarInputUsuario() { 
+function limpiarInputUsuario() {
   document.getElementById("amigo").value = "";
 }
+
+document.getElementById("amigo").addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    agregarAmigo(); 
+  }
+});
